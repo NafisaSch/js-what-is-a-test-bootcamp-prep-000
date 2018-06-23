2172b550ca864c3577237df599e53473e41a3c70
Nafisa Schofield <schofield.n.nyc@gmail.com>
2018-06-23 21:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
-var name = "JoeJoe"
-var age = 75;
+var name = "Joeee"
+var age = "75";
 //var message = `${name} is ${height} inches tall`
 var message = `${name} is ${age} year old Brooklyn's cow. U-ha-ha!`;
-module.exports = {name, height, message};
+module.exports = {name, age, message};
 console.log(message);
