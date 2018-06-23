52852daa48482dc3b7dfc2588315af3dc8c6fcc3
Nafisa Schofield <schofield.n.nyc@gmail.com>
2018-06-23 21:00:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var name = "JoeJoe"
 var age = 7
 var message = `${name} is ${height} inches tall`
-var message = `${name} is ${age} old caw
+var message = `${name} is ${age} old Brooklyn's cow. 
 module.exports = {name, height, message}
 console.log(message);
